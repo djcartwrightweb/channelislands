b467fe3699294ecece0a2cff738419b6677f0a25
DJ Cartwright <dj.cartwright.web@gmail.com>
2025-02-04 13:26:21 -0500

    Initial Commit

---
+++ ChannelIslands/ChannelIslandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChannelIslandsApp.swift
+//  ChannelIslands
+//
+//  Created by David Cartwright on 2025-02-04.
+//
+
+import SwiftUI
+
+@main
+struct ChannelIslandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChannelIslands/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChannelIslands
+//
+//  Created by David Cartwright on 2025-02-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
